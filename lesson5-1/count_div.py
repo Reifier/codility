d73cd400d14c0a98621d6489b6dc840652ecbe90
Reifier <apinchuk.soft@gmail.com>
2018-02-11 18:30:15 -0800

    Solved for additional test cases

--- lesson5-1/count_div.py
+++ lesson5-1/count_div.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import logging
 import math
@@ -7,9 +7,17 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("count_div")
 
 def solution(A, B, K):
+  if B < K:
+    logger.info("The check revealed that number B which is %d is less than number K which is %d", B, K)
+    return 0
   logger.info("The integer A is %d. Integer B is %d. Integer K is %d", A, B, K)
-  zero_factor = 0 if A == 0 else 1
-  logger.info("This is what will be added to the end result %d", zero_factor)
-  result = math.ceil((B - A)/K) + zero_factor
+  if A == B:
+    logger.info("Our range only consists of one number %d, thus testing if it is divisible by %d", B, K)
+    result = 1 if B % K == 0 else 0
+    return result
+  lowest_number = A if A % K == 0 else A + K - A
+  highest_number = B if B % K == 0 else B - B % K
+  logger.info("The lowest divisible number of the set is %d. The highest divisible number of the set is %d", lowest_number, highest_number)
+  result = (highest_number / K - lowest_number / K) + 1
   logger.info("The result is %d", result)
   return result
--- lesson5-1/ut_count_div.py
+++ lesson5-1/ut_count_div.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from count_div import *
 import unittest
 
 class TestCountDiv(unittest.TestCase):
   def testExample(self):
-    self.assertEqual(2, solution(6, 11, 3))
+    self.assertEqual(3, solution(6, 11, 2))
 
   def testWithNine(self):
     self.assertEqual(3, solution(1, 9, 3))
@@ -13,9 +13,18 @@ class TestCountDiv(unittest.TestCase):
   def testTinyRange(self):
     self.assertEqual(0, solution(0, 1, 2))
 
+  def testExtremeIfempty(self):
+    self.assertEqual(1, solution(10, 10, 5))
+    self.assertEqual(0, solution(10, 10, 7))
+    self.assertEqual(0, solution(10, 10, 20))
 
+  def testRegularExample(self):
+    self.assertEqual(20, solution(11, 345, 17))
 
+  def testPolarExtremes(self):
+    self.assertEqual(2, solution(0, 2000, 2000))
 
-
+  def test(self):
+    self.assertEqual(3, solution(10, 14, 2))
 
 unittest.main()
